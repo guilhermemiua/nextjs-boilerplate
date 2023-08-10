1f5ad2ff9b774dfa8ffdd55234c0758af3965e61
Guilherme Eiti Akita Miua <guilhermemiua@gmail.com>
2023-08-10 00:04:22 -0300

    feat: add button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Button variant="default" size={'lg'}>
+        TEST
+      </Button>
+      <Button variant="destructive">TEST</Button>
+      <Button variant="ghost">TEST</Button>
+    </>
+  );
 }
